2a37df4859d77c7e9e42014b27cf3270b92e8c27
Joanna Otmianowska <joanna.otmianowska@olx.pl>
2022-02-17 20:46:28 +0100

    posts shown as dynamic routes

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData, Link } from "remix";
 
-import { getPosts } from "~/posts";
-import type { Post } from "~/posts";
+import { getPosts } from "~/post";
+import type { Post } from "~/post";
 
 export const loader = async () => {
     return getPosts();
